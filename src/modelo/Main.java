e12893577537f23ebfb883b5db171411f010f7b1
Elio Galindo Fonseca <eliogalindo92@yahoo.com>
2021-12-31 09:04:53 -0600

    Actualizando.

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -3,8 +3,11 @@ package modelo;
 public class Main {
 
     public static void main(String[] args) {
+    //Testeando.
     TrabajadorFijo trabajadorFijo = new TrabajadorFijo("920929", "Elio","Galindo", 545.00, 7);
+    TrabajadorContratado trabajadorContratado = new TrabajadorContratado("901202", "Andrés","Fonseca", 1000.00, 300);
     RecursosHumanos.getRecursosHumanos().getTrabajadores().add(trabajadorFijo);
+    RecursosHumanos.getRecursosHumanos().getTrabajadores().add(trabajadorContratado);
 	int total = RecursosHumanos.getRecursosHumanos().mayorAntiguedad(5);
     double salarioTotal = RecursosHumanos.getRecursosHumanos().salarioTotal();
     System.out.println("El total de trabajadores con mayor antigüedad que un valor dado es: " + total);
